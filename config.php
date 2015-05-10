3779e31e94536602e33da124146c290a48d72e2b
Aleuck <aleuck@github.com>
2015-05-10 18:08:31 -0300

    configuração inicial

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php //>
+
+$config['mysql_hostname'] = 'localhost';
+$config['mysql_username'] = 'genrec';
+$config['mysql_password'] = 'senha_muito_aleatoria_wtf';
+$config['mysql_database'] = 'genrec';
---
+++ loader.php
@@ -0,0 +1,2 @@
+<?php //>
+$database = new mysqli($config['mysql_hostname'], $config['mysql_username'], $config['mysql_password'], $config['mysql_database']);
