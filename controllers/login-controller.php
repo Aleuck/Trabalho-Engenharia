d622b26f6cac9354958326ce551c524a99e9f344
nidejelski <nidejelski@gmail.com>
2015-05-12 14:49:10 -0300

    Update login-controller.php

--- controllers/login-controller.php
+++ controllers/login-controller.php
@@ -2,7 +2,7 @@
 switch($_REQUEST['acao']) {
     case 'logar': {
         print "<h1>Alguem tentando logar</h1>";
-        print "<p><strong>Email:</strong> {$_REQUEST['user']}</p>";
+        print "<p><strong>Usuario:</strong> {$_REQUEST['user']}</p>";
         print "<p><strong>Senha:</strong> {$_REQUEST['senha']}</p>";
     } break;
 }
