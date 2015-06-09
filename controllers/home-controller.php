156b6d7b006434010ba7eb322021f1d0969493d4
Alexandre Leuck <aleuck2004@gmail.com>
2015-06-09 10:20:25 -0300

    redirecting to login

--- controllers/home-controller.php
+++ controllers/home-controller.php
@@ -3,14 +3,7 @@ class HomeController extends MainController {
 	public $necessitaAutenticacao = false;
 	public function index() {
 		if (!$this->usuario) {
-			require_once ABSPATH . '/controllers/login-controller.php';
-			// Cria o objeto do controlador "home-controller.php"
-			// Este controlador deverá ter uma classe chamada HomeControlle
-			$loginController = new LoginController();
-			// Executa o método index()
-			$loginController->index();
-			// FIM
-			return;
+			header('Location: ' . HOME_URI . '/login/' );
 		} else {
 			header('Location: ' . HOME_URI . '/usuario/' );
 		}
